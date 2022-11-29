463d0d3a17a795b8d6e5f28689f354b42a3a27b1
abrehamhub <abrehamgebremedin12@gmail.com>
2022-11-29 19:10:20 +0300

    update

---
+++ java8interface/prog2_3/Prog3.java
@@ -0,0 +1,14 @@
+package prog2_3;
+
+public class Prog3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x= 1.27;
+		double y=3.881; 
+		double z=9.6;
+		System.out.println(Math.round(x+y+z));
+
+	}
+
+}
